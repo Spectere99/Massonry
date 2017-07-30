a1305140a7ab14c08fcd6a0d53037bd236de03ec
Spectere99 <rflowers@saber98.com>
2017-07-30 18:17:46 -0400

    Added Log4Net logging to the LookupType Controller

--- ConstructionManagementService/Controllers/LookupTypeController.cs
+++ ConstructionManagementService/Controllers/LookupTypeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Web;
@@ -9,61 +10,144 @@ using System.Web.Http.Description;
 using System.Web.Mvc;
 using ConstructionManagementService.Models;
 using ConstructionManagementData;
+using log4net;
 
 namespace ConstructionManagementService.Controllers
 {
     public class LookupTypeController : ApiController
     {
+        static ILog _log = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType
+        );
         private readonly ConstructionManagerEntities _dbContext = new ConstructionManagerEntities();
         //GET api/LookupType
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(HttpRequestMessage request)
         {
-            IEnumerable<LookupTypeModel> lookupTypeList = GetLookupTypes();
+            if (_log.IsDebugEnabled)
+                {
+                    _log.DebugFormat("Executing call in debug mode");
+                }
 
-            return this.Ok(lookupTypeList);
-        }
-
-        public IHttpActionResult Post(LookupTypeModel value)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest("Invalid data.");
-
-            _dbContext.LookupTypes.Add(new LookupType()
+            var headers = request.Headers;
+            //Check the request object to see if they passed a userId
+            if (headers.Contains("userid"))
             {
-                LookupTypeID = value.LookupTypeId,
-                LookupType1 = value.Type
-            });
-
-            _dbContext.SaveChanges();
-
-            return Ok();
-         }
-
-        public IHttpActionResult Put(LookupTypeModel value)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest("Invalid data.");
-
-            var existingLookupType =_dbContext.LookupTypes
-                .FirstOrDefault(l => l.LookupTypeID == value.LookupTypeId);
-
-            if (existingLookupType != null)
-            {
-                //if (value.LookupTypeId != id)
-                //{
-                //    return BadRequest("ID did not match actual value.");
-                //}
-                existingLookupType.LookupType1 = value.Type;
-
-                _dbContext.SaveChanges();
-            }
-            else
-            {
-                return NotFound();
+                var user = headers.GetValues("userid").First();
+                _log.InfoFormat("Handling GET request from user: {0}", user);
+                    
+                try
+                {
+                    _log.Debug("Getting Lookup Types");
+                    IEnumerable<LookupTypeModel> lookupTypeList = GetLookupTypes();
+                    var lookupTypeModels = lookupTypeList as IList<LookupTypeModel> ?? lookupTypeList.ToList();
+                    _log.DebugFormat("Lookup Types retreived Count: {0}", lookupTypeModels.Count());
+                    return Ok(lookupTypeModels);
+                }
+                catch (Exception e)
+                {
+                    _log.Error("An error occurred while getting Lookup Types.", e);
+                    return InternalServerError(e);
+                }
             }
 
-            return Ok();
+            return BadRequest("Header value <userid> not found.");
+
+
+
         }
+
+        public IHttpActionResult Post(HttpRequestMessage request, LookupTypeModel value)
+        {
+            if (_log.IsDebugEnabled)
+            {
+                _log.DebugFormat("Executing call in debug mode");
+            }
+
+            var headers = request.Headers;
+            //Check the request object to see if they passed a userId
+            if (headers.Contains("userid"))
+            {
+                var user = headers.GetValues("userid").First();
+                _log.InfoFormat("Handling POST request from user: {0}", user);
+
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid data.");
+
+                try
+                {
+                    _log.DebugFormat("Adding new LookupType (LookupTypeID: {0}, LookupType: {1})", value.LookupTypeId, value.Type);
+                    _dbContext.LookupTypes.Add(new LookupType()
+                    {
+                        LookupTypeID = value.LookupTypeId,
+                        LookupType1 = value.Type
+                    });
+
+                    _dbContext.SaveChanges();
+
+                    _log.Debug("Lookup Type Added");
+
+                    return Ok();
+                }
+                catch (Exception e)
+                {
+                    _log.Error("An error occurred while adding Lookup Types.", e);
+                    return InternalServerError(e);
+                }
+                
+            }
+
+            return BadRequest("Header value <userid> not found.");
+        }
+
+        public IHttpActionResult Put(HttpRequestMessage request, LookupTypeModel value)
+        {
+            if (_log.IsDebugEnabled)
+            {
+                _log.DebugFormat("Executing call in debug mode");
+            }
+
+            var headers = request.Headers;
+            //Check the request object to see if they passed a userId
+            if (headers.Contains("userid"))
+            {
+                var user = headers.GetValues("userid").First();
+                _log.InfoFormat("Handling PUT request from user: {0}", user);
+
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid data.");
+
+                try
+                {
+                    var existingLookupType = _dbContext.LookupTypes
+                        .FirstOrDefault(l => l.LookupTypeID == value.LookupTypeId);
+
+                    
+                    if (existingLookupType != null)
+                    {
+                        _log.DebugFormat("Updating existing LookupType (LookupTypeID: {0}, new_LookupType: {1})", existingLookupType.LookupTypeID, value.Type);
+                        existingLookupType.LookupType1 = value.Type;
+
+                        _dbContext.SaveChanges();
+                    }
+                    else
+                    {
+                        _log.DebugFormat("Lookup Type Not Found (LookupTypeID={0}", value.LookupTypeId);
+                        return NotFound();
+                    }
+
+                    _log.Debug("Lookup Type Updated");
+                    return Ok();
+                }
+                catch (Exception e)
+                {
+                    _log.Error("An error occurred while updating Lookup Types.", e);
+                    return InternalServerError(e);
+                }
+            }
+
+            return BadRequest("Header value <userid> not found.");
+        }
+
         //Helper Methods
         private IEnumerable<LookupTypeModel> GetLookupTypes()
         {
--- ConstructionManagementService/Global.asax.cs
+++ ConstructionManagementService/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 
 namespace ConstructionManagementService
 {
@@ -18,6 +19,7 @@ namespace ConstructionManagementService
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
