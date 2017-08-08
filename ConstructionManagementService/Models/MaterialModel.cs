ad7c49e0ef4d57510b8d91189eaf9f251468f494
mkhoy <mkhoy@LAPTOP-ECM9KTDG>
2017-08-08 18:36:33 -0400

    Created Material API

---
+++ ConstructionManagementService/Models/MaterialModel.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ConstructionManagementService.Models
+    {
+        public class MaterialModel
+        {
+            [JsonProperty("id")]//MaterialID 
+            public int MaterialId { get; set; }
+            [JsonProperty("vendor id")]//VendorID
+            public int VendorId { get; set; }
+            [JsonProperty("material product")]//MaterialProduct
+            public string MaterialProduct { get; set; }
+            [JsonProperty("color")]//Color
+            public string Color { get; set; }
+            [JsonProperty("category id")]//CategoryID
+            public int CategoryId { get; set; }
+            [JsonProperty("quantity")]//Quantity
+            public int Quantity { get; set; }
+            [JsonProperty("unit of measure id")]//UomID
+            public int UomId { get; set; }
+
+
+    }
+    }
---
+++ ConstructionManagementService/js/MaterialsController.js
@@ -0,0 +1,24 @@
+﻿app.controller('MaterialsController', function ($scope, $location, AdminService, ShareData) {
+    loadAllLookupTypeRecords();
+
+    function loadAllMaterialRecords() {
+        var promiseGetMaterials = AdminService.getMaterials();
+
+        promiseGetMaterials.then(function (pl) { $scope.Material = pl.data },
+            function (errorPl) {
+                $scope.error = errorPl;
+            });
+    }
+
+    //To Edit Student Information   
+    $scope.editMaterial = function (MaterialId) {
+        ShareData.value = materialId;
+        $location.path("/editLookupType");
+    }
+
+    //To Delete a Student   
+    $scope.deleteMaterial = function (materialId) {
+        ShareData.value = materialId;
+        $location.path("/deleteMaterial");
+    }
+});
