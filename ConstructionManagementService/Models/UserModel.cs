54d7f417ced0418470b01846eba2b5ddd4ba0c92
Mick <MDavis910@github.com>
2017-08-22 19:51:45 -0400

    Added User Model

---
+++ ConstructionManagementService/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+namespace ConstructionManagementService.Models
+{
+    public class UserModel
+    {
+        [JsonProperty("id")]
+        public int UserId { get; set; }
+        [JsonProperty ("user")]
+        public string UserName {get; set;}
+        [JsonProperty ("email")]
+        public string email { get; set; }
+        [JsonProperty ("firstName")]
+        public string FirstName { get; set; }
+        [JsonProperty ("lastName")]
+        public string LastName { get; set; }
+        [JsonProperty ("contactNumber")]
+        public int ContactNumber { get; set;}
+        [JsonProperty ("updated")]
+        public int LastUpdated { get; set; }
+
+    }
+}
