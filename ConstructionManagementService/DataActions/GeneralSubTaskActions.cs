e2ea804a2e09357855388d8a2a699b6beac167f5
Mick <MDavis910@github.com>
2017-09-05 20:02:49 -0400

    GeneralSubTasksActions

---
+++ ConstructionManagementService/DataActions/GeneralSubTaskActions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ConstructionManagementData;
+using ConstructionManagementService.Models;
+
+namespace ConstructionManagementService.DataActions
+{
+    public class GeneralSubTaskActions : IActions<GeneralSubTaskModel>
+    {
+        private readonly ConstructionManagerEntities _dbContext = new ConstructionManagerEntities();
+
+
+        public void Deactivate(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GeneralSubTaskModel> Get(bool showInactive)
+        {
+            try { 
+                var generalSubtasks = showInactive ? _dbContext.GeneralSubTasks.ToList() :
+                    _dbContext.GeneralSubTasks.Where(p => p.IsActive).ToList();
+                var generalSubtasksModels = generalSubtasks.Select(generalSubtask => new GeneralSubTaskModel
+                    {  Id = generalSubtask.GenSubTaskID,
+                        Name = generalSubtask.SubTaskName,
+                        Description = generalSubtask.SubTaskDescription,
+                        IsActive = generalSubtask.IsActive,
+                        Created = generalSubtask.Created,
+                        CreatedBy = generalSubtask.CreatedBy,
+                        LastUpdated = generalSubtask.LastUpdated
+                    }).ToList();
+
+                return generalSubtasksModels;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _dbContext.Dispose();
+            }
+}
+        public GeneralSubTaskModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(GeneralSubTaskModel modelObj, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GeneralSubTaskModel modelObj, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConstructionManagementService/Models/GeneralSubTaskModel.cs
+++ ConstructionManagementService/Models/GeneralSubTaskModel.cs
@@ -9,12 +9,20 @@ namespace ConstructionManagementService.Models
     public class GeneralSubTaskModel
     {
         [JsonProperty("id")]
-        public int SubTaskId { get; set; }
-        [JsonProperty("taskName")]
-        public string SubTaskName { get; set; }
-        [JsonProperty("taskDescription")]
-        public string SubTaskDesription { get; set; }
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("isActive")]
+        public bool IsActive { get; set; }
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+        [JsonProperty("createdBy")]
+        public string CreatedBy { get; set; }
         [JsonProperty("lastUpdated")]
         public DateTime LastUpdated { get; set; }
+        [JsonProperty("lastUpdatedBy")]
+        public string LastUpdatedBy { get; set; }
     }
 }
