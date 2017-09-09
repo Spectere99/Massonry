628607e8efa0d73ab33101a2a817e40c2c5e0067
Spectere99 <rflowers@saber98.com>
2017-09-08 22:33:04 -0400

    Added GeneralPlanTask Objects

---
+++ ConstructionManagementService/Models/GeneralPlanTaskModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ConstructionManagementService.Models
+{
+    public class GeneralPlanTaskModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("planId")]
+        public int PlanId { get; set; }
+        [JsonProperty("plan")]
+        public GeneralTaskModel GeneralPlan { get; set; } //CHange to GeneralPlanModel type.
+        [JsonProperty("taskId")]
+        public int TaskId { get; set; }
+        [JsonProperty("task")]
+        public GeneralTaskModel GeneralTask { get; set; }
+        [JsonProperty("isActive")]
+        public bool IsActive { get; set; }
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+        [JsonProperty("createdBy")]
+        public string CreatedBy { get; set; }
+        [JsonProperty("lastUpdated")]
+        public DateTime LastUpdated { get; set; }
+        [JsonProperty("lastUpdatedBy")]
+        public string LastUpdatedBy { get; set; }
+    }
+}
